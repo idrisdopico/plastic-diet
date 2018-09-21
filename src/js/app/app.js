15546d904ffebdc399254ee03e2bb739cf8fa707
Idris Dopico Peña <idopico@zigwebsoftware.nl>
2018-09-21 22:23:41 +0200

    small amount of js..

--- src/js/app/app.js
+++ src/js/app/app.js
@@ -1,31 +1,12 @@
-// $('.header__link').click(function (e) {
-//     e.preventDefault();
-//     $('body, html').animate({
-//         scrollTop: $('.about').offset().top
-//     }, 500);
-//     return false;
-// });
-//
-// $('.about__link').click(function (e) {
-//     e.preventDefault();
-//     $('body, html').animate({
-//         scrollTop: $('.innovation').offset().top
-//     }, 500);
-//     return false;
-// });
-//
-// $('.button--next').click(function (e) {
-//     e.preventDefault();
-//     $('body, html').animate({
-//         scrollTop: $(e.target).closest('.innovation').next('.innovation').offset().top
-//     }, 500);
-//     return false;
-// });
-//
-// $('.button--to-footer').click(function (e) {
-//     e.preventDefault();
-//     $('body, html').animate({
-//         scrollTop: $('.footer').offset().top
-//     }, 500);
-//     return false;
-// });
+window.addEventListener('DOMContentLoaded', function() {
+    const title = document.getElementById('header__heading');
+    const form = document.getElementById('form');
+
+    if(title) {
+        title.className += title.className ? ' is-active' : 'is-active';
+    }
+
+    if(form) {
+        form.className += form.className ? ' is-active' : 'is-active';
+    }
+});
